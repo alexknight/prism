1822c4c5f4bc4491877136909ff4d512f629add8
qingge <qingge@wacai.com>
2018-08-05 14:19:43 +0800

    新增performace入口

---
+++ prism/src/main/java/com/android/prism/Performance.java
@@ -0,0 +1,26 @@
+package com.android.prism;
+
+import android.content.Context;
+
+/**
+ * Project: PrismSDK
+ *
+ * @author : Alex(qingge)
+ * @date : 2018/8/5 下午2:02
+ */
+
+
+class Performance {
+    private final Context mContext;
+
+    public Performance(Context context) {
+        this.mContext = context;
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+    }
+}
--- prism/src/main/java/com/android/prism/PrismSDK.java
+++ prism/src/main/java/com/android/prism/PrismSDK.java
@@ -1,9 +1,11 @@
 package com.android.prism;
 
+import android.app.Application;
 import android.content.Context;
 
 import com.android.prism.crash.Cockroach;
 import com.android.prism.crash.CrashHandler;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by qingge on 2018/8/4.
@@ -12,9 +14,11 @@ import com.android.prism.crash.CrashHandler;
 public class PrismSDK {
     private static volatile PrismSDK sPrismSDK;
     private Context context;
+    private Performance mPerformance;
 
     private PrismSDK (Context context){
         this.context = context;
+        mPerformance = new Performance(context);
     }
 
     public static PrismSDK getInstance(Context context) {
@@ -32,6 +36,15 @@ public class PrismSDK {
     public void start() {
         // crash处理<->ui线程looper中
         Cockroach.install(new CrashHandler(this.context));
+        // 内存泄漏
+        LeakCanary.install((Application) this.context);
+        // 开启内存，CPU，帧率监控
+        mPerformance.start();
+    }
+
+    public void stop(){
+        Cockroach.uninstall();
+        mPerformance.stop();
     }
 
 }
